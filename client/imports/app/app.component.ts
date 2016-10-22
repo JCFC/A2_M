18a3a0b977c4027ff7671d5d34055a4e6ae42036
jchen42 <je.christophe.an@gmail.com>
2016-10-22 10:57:09 +0200

    chap 4 ok

---
+++ client/imports/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Party } from '../../../both/models/party.model';
+import { Parties } from '../../../both/collections/parties.collection';
+
+import template from './app.component.html';
+
+
+@Component({
+  selector: 'app',
+  template
+})
+
+export class AppComponent {
+	parties: Observable<Party[]>;
+ 
+	constructor() {
+		this.parties = Parties.find({}).zone(); //zone?
+	}
+
+	removeParty(party: Party): void {
+		Parties.remove(party._id);
+	}
+}
---
+++ start_meteor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+MONGO_URL="mongodb://cjcf:adminadmin@ds048279.mlab.com:48279/matcha_" meteor
+
